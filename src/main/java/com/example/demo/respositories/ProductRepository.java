6ad55206429b2b2774e0cacc8c12396011e7a3a4
jjguillen <josejavierguillen@gmail.com>
2025-01-24 16:45:17 +0100

    Primer commit

--- src/main/java/com/example/demo/respositories/ProductRepository.java
+++ src/main/java/com/example/demo/respositories/ProductRepository.java
@@ -12,4 +12,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findProductsByTitleContaining(String title);
     List<Product> findProductsByPriceGreaterThan(Double price);
 
+
 }
